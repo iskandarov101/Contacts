d4056cc94a3462c84e4ba837023d120bef5b36a5
iskandarov101 <buriboyiskandarov@gmail.com>
2021-04-17 14:04:41 +0500

    initial comment

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var $_ = function (selector, node = document) {
+  return node.querySelector(selector);
+};
+
+// var elIdish = $_('.idish');
+// var elButton = $_('button', elIdish);
+
+var $$_ = function (selector, node = document) {
+  return node.querySelectorAll(selector);
+};
+
+var createElement = function (element, elementClass, text) {
+  var newElement = document.createElement(element);
+
+  if (elementClass) {
+    newElement.setAttribute('class', elementClass);
+  }
+
+  if (text) {
+    newElement.textContent = text;
+  }
+
+  return newElement;
+};
