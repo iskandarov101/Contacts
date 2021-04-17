2adf781645e50cb56961d713076965f7e223501e
iskandarov101 <buriboyiskandarov@gmail.com>
2021-04-17 14:53:23 +0500

    add phoneNumber function

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,38 @@ var createElement = function (element, elementClass, text) {
 
   return newElement;
 };
+
+
+let elForm = document.querySelector('.js-contact-form');
+let elInput = document.querySelector('.js-contact-form__name-input');
+let elRelationship = document.querySelector('.js-contact-form__relationship-input');
+let elPhone = document.querySelector('.js-contact-form__phone-input');
+
+//  OUTPUT
+let elNameOutput = document.querySelector('.js-contact__name');
+let elRelationshipOutput = document.querySelector('.js-contact__relationship');
+let elPhoneOutput = document.querySelector('.js-contact__phone');
+
+
+
+
+
+// elForm.addEventListener('submit', function(evt) {
+//   evt.preventDefault();
+
+//   let phoneNumber = elPhone.value;
+
+//   let phoneLink = document.createElement('a');
+//   phoneLink.classList.add('border-bottom', 'border-warning', 'text-info', 'h4');
+//   phoneLink.href = `tel: ${phoneNumber}`;
+//   phoneLink.textContent = phoneNumber;
+
+//   elPhoneOutput.appendChild(phoneLink);
+//   elPhone.value = '';
+// });
+
+
+
+
+
+
