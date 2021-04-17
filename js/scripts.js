bc8c8de8a06f5a28485710b0e4210f8817462722
iskandarov101 <buriboyiskandarov@gmail.com>
2021-04-17 15:16:59 +0500

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -34,23 +34,53 @@ let elNameOutput = document.querySelector('.js-contact__name');
 let elRelationshipOutput = document.querySelector('.js-contact__relationship');
 let elPhoneOutput = document.querySelector('.js-contact__phone');
 
+function NameInput() {
+  elNameOutput.classList.add('px-3', 'py-4', 'rounded')
+  let contactOutput = document.createElement('p');
+  contactOutput.classList.add('h5', 'text-secondary');
+  contactOutput.textContent = elInput.value;
+
+  elNameOutput.appendChild(contactOutput);
+  elInput = '';
+}
+
+function relationshipInput(){
+  
+  elRelationshipOutput.classList.add('h5', 'text-secondary');
+
+  // let qarindosh = elRelationship.value;
+
+  let relationshipOutput = docement.querySelector('p');
+  relationshipOutput.classList.add('h5', 'text-secondary');
+  relationshipOutput.textContent = elRelationship.value;
+
+  elRelationshipOutput.appendChild(relationshipOutput);
+  elRelationship.value = '';
+}
+
+function phoneInput() {
+  let phoneNumber = elPhone.value;
+
+  let phoneLink = document.createElement('a');
+  phoneLink.classList.add('border-bottom', 'border-warning', 'text-info', 'h4');
+  phoneLink.href = `tel: ${phoneNumber}`;
+  phoneLink.textContent = phoneNumber;
+
+  elPhoneOutput.appendChild(phoneLink);
+  elPhone.value = '';
+}
 
 
+elForm.addEventListener('submit', function(evt){
+  evt.preventDefault();
+
+  NameInput();
+  relationshipInput();
+  phoneInput();
+ 
+});
 
 
-// elForm.addEventListener('submit', function(evt) {
-//   evt.preventDefault();
-
-//   let phoneNumber = elPhone.value;
-
-//   let phoneLink = document.createElement('a');
-//   phoneLink.classList.add('border-bottom', 'border-warning', 'text-info', 'h4');
-//   phoneLink.href = `tel: ${phoneNumber}`;
-//   phoneLink.textContent = phoneNumber;
-
-//   elPhoneOutput.appendChild(phoneLink);
-//   elPhone.value = '';
-// });
 
 
 
